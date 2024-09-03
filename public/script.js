48107173d833a0a7c306d1b28a1c7a479be872d8
tasya2009 <tasyap1234@gmail.com>
2024-09-03 18:27:21 +0530

    class220

--- public/script.js
+++ public/script.js
@@ -12,22 +12,75 @@ const myvideo = document.createElement("video")
 myvideo.muted = true;
 let mystream;
 navigator.mediaDevices.getUserMedia({
-    audio : true, 
-    video : true,
+    audio: true,
+    video: true,
 })
-.then((straem) => {
-    mystream = straem;
-    addVideoStream(myvideo,straem);
-})
-function addVideoStream(video, straem){
+    .then((straem) => {
+        mystream = straem;
+        addVideoStream(myvideo, straem);
+        socket.on("user-connected", (userId) => {
+            connectToUser(userId, straem);
+        })
+        peer.on("call", (call) => {
+            call.answer(straem);
+            const video = document.createElement("video");
+            call.on('stream', (userstream) => {
+                addVideoStream(video, userstream)
+            })
+        })
+    })
+
+function connectToUser(userId, straem) {
+    const call = peer.call(userId, straem);
+    const video = document.createElement("video");
+    call.on('stream', (userstream) => {
+        addVideoStream(video, userstream)
+    })
+}
+
+
+
+function addVideoStream(video, straem) {
     video.srcObject = straem;
-    video.addEventListener("loadedmetadata", ()=>{
+    video.addEventListener("loadedmetadata", () => {
         video.play();
         $("#video_grid").append(video)
     })
 }
 
 $(function () {
+    $('#mute_button').click(function (){
+        const enabled = mystream.getAudioTracks()[0].enabled;
+        if(enabled){
+            mystream.getAudioTracks()[0].enabled = false;
+            html = `<i class="fa fa-microphone-slash"></i>`
+            $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html)
+        }
+        else{
+            mystream.getAudioTracks()[0].enabled = true;
+            html = `<i class="fa fa-microphone"></i>`
+            $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html)
+        }
+    })
+
+    $('#stop_video').click(function (){
+        const enabled = mystream.getVideoTracks()[0].enabled;
+        if(enabled){
+            mystream.getVideoTracks()[0].enabled = false;
+            html = `<i class="fa fa-video-slash"></i>`
+            $("#stop_video").toggleClass("background_red");
+            $("#stop_video").html(html)
+        }
+        else{
+            mystream.getVideoTracks()[0].enabled = true;
+            html = `<i class="fa fa-video"></i>`
+            $("stop_video").toggleClass("background_red");
+            $("stop_video").html(html)
+        }
+    })
+
     $("#show_chat").click(function () {
         $(".left-window").css("display", "none")
         $(".right-window").css("display", "block")
--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.get("/:room", (req, res) => {
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId, userName) => {
         socket.join(roomId);
+        io.to(roomId).emit("user-connected", userId);
         socket.on("message", (message) => {
             io.to(roomId).emit("createMessage", message, userName);
         });
